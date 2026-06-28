e1d434ec5eaa7ac99f0321df15561b97d28ccd2d
Heiner Ariel Zambrano Ronquillo <hzambranor@uteq.edu.ec>
2026-06-28 01:46:32 -0500

    chore: actualiza configuración global frontend, estilos y fuente

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ToastContainerComponent } from './shared/ui/toast/toast';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToastContainerComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
